1330d321782829e541de5825a40b28516bd175db
tbalint19 <toth910719balint@gmail.com>
2017-09-02 11:01:37 +0200

    routes for check

--- Application/Backend/API/App_Profile/urls.py
+++ Application/Backend/API/App_Profile/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
 
+    url(r'^check_username$', views.check_username, name='check_username'),
+    url(r'^check_email$', views.check_email, name='check_email'),
+
     url(r'^signup$', views.signup_user, name='signup_user'),
     url(r'^login$', views.login_user, name='login_user'),
 
